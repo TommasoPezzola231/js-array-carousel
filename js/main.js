5f89ba95fce0f856032d0e8a5ea8e957a93d2b6e
TommasoPezzola <pezzola123@gmail.com>
2023-04-19 16:13:11 +0200

    Milestone 2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,25 @@
-let container = document.getElementsByClassName("container");
-let carousel = document.getElementsByClassName("carousel");
-let immagine = document.getElementsByTagName("img");
+let container = document.getElementById("container");
+let carousel = document.getElementById("carousel");
+let immagine = document.querySelector("img");
 let buttonNext = document.getElementById("next");
 let buttonPrevious = document.getElementById("previous");
+let n = 1;
 
-let nuovaImmagine = document.createElement(`img`);
-nuovaImmagine.className = "dBlock";
-nuovaImmagine.src = "../img/01.webp"
-carousel.append("nuovaImmagine")
+for (let i = 0; i < 5; i++) {
+    
+    let nuovaImmagine = document.createElement(`img`);
+
+    if ( i < 1) {
+        nuovaImmagine.className = "dBlock";
+    } else {
+        nuovaImmagine.className = "dNone";
+    }
+
+     nuovaImmagine.src = `./img/0${n}.webp`;
+    carousel.prepend(nuovaImmagine);
+
+    n++
+
+}
+
+
